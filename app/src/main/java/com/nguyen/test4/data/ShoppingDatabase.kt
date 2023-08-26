d7d0b6b324d9ea66ad161d2a9353ee0b4511448b
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2023-08-25 17:26:13 -0700

    Setting up Project & Room DB - Testing on Android - Part 5

---
+++ app/src/main/java/com/nguyen/test4/data/ShoppingDatabase.kt
@@ -0,0 +1,9 @@
+package com.nguyen.test4.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShoppingItem::class], version = 1)
+abstract class ShoppingDatabase : RoomDatabase() {
+    abstract fun shoppingDao(): ShoppingDao
+}
